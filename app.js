bc4c1657cda76e236872d1676bd1e7928371e4b2
rileyfroehlich <rileyfro2@gmail.com>
2021-02-24 17:08:58 -0800

    trying to update heroku :(

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.cookieParser('IxD secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
