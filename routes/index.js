028a86a34897787432b4635a59585df9800e4dba
rileyfroehlich <rileyfro2@gmail.com>
2021-02-24 17:55:54 -0800

    Maybe missing semi-colons?

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@
 var data = require('../lesson_data.json')
 
 exports.viewHome = function(req, res){
-  res.render('home', data)
+  res.render('home', data);
 };
 
 exports.viewLogin = function(req, res){
-  res.render('login', data)
+  res.render('login', data);
 };
 
 exports.viewQuestion = function(req, res){
 	var projectID = req.params.lesson;
 	var split = projectID.split(".");
-	projectID = split[1]
+	projectID = split[1];
 	projectID = parseInt(projectID) - 1;
 
-    res.render('question', data['projects'][projectID])
-}
+    res.render('question', data['projects'][projectID]);
+};
