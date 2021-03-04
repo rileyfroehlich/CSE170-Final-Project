a224483667a2f945f3425eb6368fb43c7dda6d06
rileyfroehlich <rileyfro2@gmail.com>
2021-03-03 23:42:46 -0800

    Log out button fully functional

---
+++ public/js/log_out_button.js
@@ -0,0 +1,22 @@
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	initLogout()
+}
+
+function initLogout(e) {
+	$('.log-out-button').click(function(e)	{
+
+		window.location.replace("https://a7-doctorate-dre.herokuapp.com/")
+
+		//<<<<<<<<<<<<<UNCOMMENT FOR LOCAL TESTING>>>>>>>>>>>>>
+		//window.location.replace("http://localhost:3000/")
+		return false
+	})
+}
